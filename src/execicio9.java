1b86405f077e272cdbd84bc9c356d4a06326ded3
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-26 20:40:13 -0300

    exercicio 09 finalizado

--- src/execicio9.java
+++ src/execicio9.java
@@ -6,7 +6,31 @@ public class execicio9 {
 
         // declaração de variavel
 
-        int valor1;
+        int valordousuario;
+        int unidade;
+        int dezena;
+        int centena;
+        int valorfinal;
+
+        // entrada de dados
+        System.out.println("Digite um valor com 3 digitos");
+        valordousuario = sc.nextInt();
+
+        //processamento
+
+         unidade= valordousuario%10;
+         dezena = valordousuario/10 %10;
+         centena = valordousuario/100;
+         valorfinal = unidade * 100 + dezena * 10 + centena;
+
+
+
+         //saida de dados
+        System.out.println("Valor invertido: " + valorfinal);
+        System.out.println(unidade + "" + dezena + "" + centena);
+
+
+
 
     }
 }
